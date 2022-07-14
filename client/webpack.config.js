7de8b6997b51a9709af837c72b57a40d11be4b67
Destiny Henry <Destiny@Desirees-MBP.fios-router.home>
2022-07-13 20:06:16 -0400

    adding in css loader and babel to webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -25,6 +25,25 @@ module.exports = () => {
 
     module: {
       rules: [
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options:{
+              presets: ["@babel/present-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime"
+              ]
+
+            }
+          }
+        }
         
       ],
     },
